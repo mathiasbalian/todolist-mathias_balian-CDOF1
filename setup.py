431dd268e6753e0e7335132f4c36f7dacc9a1b29
mathiasbalian <mathias.balian92@gmail.com>
2024-01-22 09:30:31 +0100

    created setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name='Todolist',
+    version='1.0',
+    author='mathiasbalian',
+    license='MIT',
+    long_description=open('README.md').read(),
+)
