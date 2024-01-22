42b49f527706036ca86af1189309543c722e91d3
mathiasbalian <mathias.balian92@gmail.com>
2024-01-22 10:07:41 +0100

    added actions reading

--- main.py
+++ main.py
@@ -6,4 +6,21 @@ todos = []
 print("---------- Welcome to the Todo App! ----------")
 while True:
     display_actions()
+    action = input('')
+    check_action(action)
 
+    if action == '1':
+        display_todos(todos)
+
+    elif action == '2':
+        add_todo(todos)
+
+    elif action == '3':
+        change_todo_status(todos)
+
+    elif action == '4':
+        delete_todo(todos)
+
+    elif action == '5':
+        print('---------- Thank you for using the Todo App! ----------')
+        break
