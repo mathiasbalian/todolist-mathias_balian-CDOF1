81b366a1bfc0a8c51a22a92841237e6be8111cc7
mathiasbalian <mathias.balian92@gmail.com>
2024-01-22 10:28:30 +0100

    modified display and change_status function logic

--- Todo.py
+++ Todo.py
@@ -5,14 +5,13 @@ class Todo:
         self.completed = completed
 
     def __str__(self):
-        return f'{self.id} | {self.title} | {self.status}'
+        return f'{self.id} | {self.title} | Completed: {self.completed}'
 
     def change_status(self, new_status):
-        if new_status is not bool:
+        if type(new_status) is not bool:
             print('Status must be a boolean value (True or False).')
             return
 
         self.completed = new_status
-
-
-
+        print('Todo status changed successfully.')
+        print('------------------------------------------')
