93fc9e58d7cb10f847b44a2b2e9ae968c9a87108
mathiasbalian <mathias.balian92@gmail.com>
2024-01-22 09:50:56 +0100

    created tools file

---
+++ Tools.py
@@ -0,0 +1,6 @@
+def display_actions():
+    print('1. Add a new todo')
+    print('2. List all todos')
+    print('3. Change a todo status')
+    print('4. Delete a todo')
+    print('What would you like to do ?')
